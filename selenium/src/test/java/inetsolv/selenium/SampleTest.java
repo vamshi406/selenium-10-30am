b8070ab72e633875cdd7f4c08743370472b65317
sudheer <sudheer@192.168.0.3>
2018-02-01 11:57:45 +0530

    new sampletest case added

---
+++ selenium/src/test/java/inetsolv/selenium/SampleTest.java
@@ -0,0 +1,19 @@
+package inetsolv.selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class SampleTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
